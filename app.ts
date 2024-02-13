c374472d0967f51c6dd2cbefbc2085545d8e8379
unknown <prachimsr1998@gmail.com>
2024-02-13 16:08:34 +0530

    first commit

---
+++ app.ts
@@ -0,0 +1,32 @@
+// for string
+let Name:string='prachi'
+console.log('Name is:,',Name);
+
+// for number
+let a:number=10;
+let b:number=20;
+const sum:number=a+b;
+console.log('sum is :',sum);
+
+// for sring
+let str:string ="prachi";
+const cnst: string =str + " How are you";
+console.log("stringis:",cnst);
+
+// for boolean
+let variable1:boolean=true;
+const isTrue:boolean=variable1 && true;
+console.log("Boolean is:",isTrue);
+
+// for indexing
+enum  Color {Red, Green, Blue};
+
+console.log("color is: ",Color)
+let selectedColor:Color=Color.Red;
+console.log("color is:",selectedColor);
+
+// for null undefined value
+let abc:null=null;
+let xyz:undefined=undefined;
+console.log("abc is:",abc);
+console.log("xyz is:",xyz);
